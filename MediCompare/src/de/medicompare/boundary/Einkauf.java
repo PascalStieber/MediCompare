e31f455b2276e4f1643b26860614336d2acc1f2c
Pascal Stieber <pascal.stieber.ps@googlemail.com>
2015-04-27 21:39:33 +0200

    some monday work :-/

--- MediCompare/src/de/medicompare/boundary/Einkauf.java
+++ MediCompare/src/de/medicompare/boundary/Einkauf.java
@@ -1,25 +1,34 @@
 package de.medicompare.boundary;
 
 import javax.ejb.Stateful;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import de.medicompare.control.MedikamentControl;
 import de.medicompare.entities.Medikament;
 
 @Stateful
 @Named("einkauf")
 public class Einkauf {
-
+	
+	@Inject
+	private MedikamentControl medikamentControl; 
 	private Medikament medikament;
 	
 	public Medikament getMedikament(){
 		return this.medikament;
 	}
 	
-	public String getAdminLink(){
-		return "/admin/index.xhtml?faces-redirect=true";
+	public void speicherMedikament(){
+		medikamentControl.saveMedikament(this.medikament);
 	}
+	
 	
 	public String getEinkaufsliste(){	
 		return "leer";
+	}
+	
+	public String getAdminLink(){
+		return "/admin/index.xhtml?faces-redirect=true";
 	}
 }
